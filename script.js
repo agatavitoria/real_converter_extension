9f97b5fb57089d8b12f3d834dc4a4c8aa37b0a60
Agata Vitoria <agatavitoriadelima@gmail.com>
2023-03-28 17:42:10 -0300

    created method to get from api

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const spanDolar = document.querySelector('.dolar__value');
 const spanReal = document.querySelector('.real__value');
 
-let totalDolar = 5.17;
-let totalReal = 1;
+const toReal = new Intl.NumberFormat('pt-BR', {
+    currency: 'BRL',
+});
 
-window.onload = () => {
-    let toReal = new Intl.NumberFormat('pt-BR', {
-        currency: 'BRL',
-    });
+let totalDolar = '';
+let totalReal = '';
 
+const roundValue = (num) => {
+    return Math.round(num * 100) / 100
+}
+
+const handleValue = () => {
     spanDolar.innerHTML = totalReal;
     spanReal.innerHTML = toReal.format(totalDolar);
 }
+
+const fetchApi = async () => {
+    try {
+        const response = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL')
+        const { USDBRL } = await response.json();   
+
+        totalReal = 1;
+        totalDolar = roundValue(USDBRL.bid);
+
+        handleValue();
+    } catch (err) {
+        console.log(err);    
+    }
+}
+
+window.onload = () => fetchApi();
